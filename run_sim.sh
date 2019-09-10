80bdc28436c2383f5694829a5b2ab24dafa08140
Mark Jones <mark.jones@telethonkids.org.au>
2019-09-10 11:43:49 -0500

    Referencing wrong script

--- run_sim.sh
+++ run_sim.sh
@@ -1,5 +1,5 @@
 #!/sbin/bash
 
-/usr/bin/Rscript main.R -s 1 -n 1000 > log/sim1.log 2>&1 &
-/usr/bin/Rscript main.R -s 2 -n 1000 > log/sim1.log 2>&1 &
+/usr/bin/Rscript design_01.R -s 1 -n 1000 > log/sim1.log 2>&1 &
+/usr/bin/Rscript design_01.R -s 2 -n 1000 > log/sim2.log 2>&1 &
 
