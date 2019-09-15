3caa488094b4f509afa172034d113467a0b4c321
Mark Jones <mark.jones@telethonkids.org.au>
2019-09-15 03:30:16 -0500

    Added new scen

--- run_sim.sh
+++ run_sim.sh
@@ -3,9 +3,12 @@
 # /usr/bin/Rscript design_01.R -s 1 -n 1000 > log/sim1.log 2>&1 &
 # /usr/bin/Rscript design_01.R -s 2 -n 1000 > log/sim2.log 2>&1 &
 
-/usr/bin/Rscript design_02.R -s 1 -n 1000 > log/des02_scen1_sim.log 2>&1 &
-/usr/bin/Rscript design_02.R -s 2 -n 1000 > log/des02_scen2_sim.log 2>&1 &
-/usr/bin/Rscript design_02.R -s 3 -n 1000 > log/des02_scen3_sim.log 2>&1 &
-/usr/bin/Rscript design_02.R -s 4 -n 1000 > log/des02_scen4_sim.log 2>&1 &
-/usr/bin/Rscript design_02.R -s 5 -n 1000 > log/des02_scen5_sim.log 2>&1 &
+# s is scenario
+# n is number of simulations
+/usr/bin/Rscript design_02.R -s 1 -n 2000 > log/des02_scen1_sim.log 2>&1 &
+/usr/bin/Rscript design_02.R -s 2 -n 2000 > log/des02_scen2_sim.log 2>&1 &
+/usr/bin/Rscript design_02.R -s 3 -n 2000 > log/des02_scen3_sim.log 2>&1 &
+/usr/bin/Rscript design_02.R -s 4 -n 2000 > log/des02_scen4_sim.log 2>&1 &
+/usr/bin/Rscript design_02.R -s 5 -n 2000 > log/des02_scen5_sim.log 2>&1 &
+/usr/bin/Rscript design_02.R -s 6 -n 2000 > log/des02_scen6_sim.log 2>&1 &
 
